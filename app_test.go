d881767682bd961c6e44c02cf90b0e0113426472
Pierre Beaucamp <mail@pierrebeaucamp.com>
2015-06-20 15:01:16 +0200

    added tests

---
+++ app_test.go
@@ -0,0 +1,19 @@
+package gotobi
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestHandleIndexReturnsWithStatusOk(t *testing.T) {
+	req, _ := http.NewRequest("GET", "/", nil)
+	res := httptest.NewRecorder()
+
+	index(res, req)
+
+	if res.Code != http.StatusOK {
+		t.Fatalf("Response body did not contain expected %v:\n\tbody: %v",
+			"200", res.Code)
+	}
+}
