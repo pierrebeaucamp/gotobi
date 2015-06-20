f39e7e7516c05977c8bef3c44b5abdb2ee937600
Pierre Beaucamp <mail@pierrebeaucamp.com>
2015-06-20 17:48:22 +0200

    changed folder structure again

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 [![wercker status](https://app.wercker.com/status/712d8ed24acf24011e08a8c9309cfa70/m "wercker status")](https://app.wercker.com/project/bykey/712d8ed24acf24011e08a8c9309cfa70)
 
-Be sure to set the correct $GOPATH before building. I recommend  ``export GOPATH=$(pwd)``
+As this application has a couple of subpackages we need to set a special ``GOPATH``. This repository needs to be in ``src/github.com/pierrebeaucamp/gotobi`` and ``GOPATH`` needs to be set to the directoy which contains ``src/github.com/...``
---
+++ controllers/index.go
@@ -0,0 +1,25 @@
+package controllers
+
+import (
+	"html/template"
+	"net/http"
+)
+
+func getTemplate(name string) *template.Template {
+	file := "src/html/" + name + ".html"
+	return template.Must(template.New("").Funcs(nil).ParseFiles(file,
+		"src/html/base.html"))
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	t := getTemplate("index")
+	render(t, w, nil)
+}
+
+func render(t *template.Template, w http.ResponseWriter,
+	varmap map[string]interface{}) {
+	err := t.ExecuteTemplate(w, "body", varmap)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
