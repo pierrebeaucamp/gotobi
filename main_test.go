54400d0093e31b4f9704658b862020625774a25e
Pierre Beaucamp <mail@pierrebeaucamp.com>
2015-06-20 16:21:29 +0200

    updated tests

--- main_test.go
+++ main_test.go
@@ -3,17 +3,25 @@ package main
 import (
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
-func TestHandleIndexReturnsWithStatusOk(t *testing.T) {
+func TestIndex(t *testing.T) {
 	req, _ := http.NewRequest("GET", "/", nil)
 	res := httptest.NewRecorder()
 
 	index(res, req)
 
+	// Test if we get 200
 	if res.Code != http.StatusOK {
 		t.Fatalf("Response body did not contain expected %v:\n\tbody: %v",
 			"200", res.Code)
+	}
+
+	body := res.Body.String()
+	if !strings.Contains(body, "<!DOCTYPE html>") {
+		t.Fatalf("Response body did not contain expected %v:\n\tbody: %v",
+			"<!DOCTYPE html>", body)
 	}
 }
