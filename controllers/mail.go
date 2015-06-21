24d1c80797b1c0b75ed79a90b05ba6f7df722a78
Pierre Beaucamp <mail@pierrebeaucamp.com>
2015-06-21 02:36:19 +0200

    Further Sendgrid testing

--- controllers/mail.go
+++ controllers/mail.go
@@ -6,10 +6,11 @@ import (
 )
 
 func InboundMail(w http.ResponseWriter, r *http.Request) {
-	if r.FormValue("from") == "" && r.FormValue("subject") == "" {
+	if r.FormValue("from") == "" && r.FormValue("to") == "" {
 		fmt.Fprint(w, "This is not an Email")
 	}
 
 	fmt.Println(r.FormValue("from"))
 	fmt.Println(r.FormValue("subject"))
+	fmt.Println(r.FormValue("to"))
 }
